3326eafd9536796a6eaa642484de73dd4d5a4c0d
karthick <karthick4in@gmail.com>
2023-01-12 01:33:11 +0530

    code opt1

--- src/javascriptView.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from './Layout';
-
-export default () => (
-  <Layout>
-    <h1>r/javascript</h1>
-  </Layout>
-);
--- src/learnreactjsView.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from './Layout';
-
-export default () => (
-  <Layout>
-    <h1>r/learnreactjs</h1>
-  </Layout>
-);
